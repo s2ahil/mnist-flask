41cc26eae6de1ee3ae2068bddefe5ca6dd998678
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-19 00:29:36 +0530

    Update mnist_backend.py

--- mnist_backend.py
+++ mnist_backend.py
@@ -7,51 +7,32 @@ import pickle
 import tensorflow.compat.v2 as tf
 from keras.models import model_from_json
 
-
 app = Flask(__name__)
 
-# with open('./mnist.pkl', 'rb') as model_file:
-#     model = pickle.load(model_file)
-# model = joblib.load('mnist.pkl')
-# model = load_model('mnist_h5')
-# json_file = open('myModel.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# new_model = model_from_json(loaded_model_json)
-
-
-@app.route('/',methods = ['GET'])
-# ‘/’ URL is bound with hello_world() function.
+@app.route('/', methods=['GET'])
 def hello_world():
-	json_file = open('myModel.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        new_model = model_from_json(loaded_model_json)
-	# Read the uploaded image
-        image_path = '8.png'  # Replace with the path to your image
-        def preprocess():
-         gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-         # Resize the image to 28x28 pixels (MNIST size)
-         resized_image = cv2.resize(gray_image, (28, 28))
-         # Invert the colors (black to white, white to black)
-         inverted_image = cv2.bitwise_not(resized_image)
+    json_file = open('myModel.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    new_model = model_from_json(loaded_model_json)
 
-         # Display the inverted grayscale image with 'gray' colormap
- # plt.figure(figsize=(5, 5))
- # plt.title('Inverted Grayscale Image')
- # plt.imshow(inverted_image, cmap='gray')  # Use 'gray' colormap
- # plt.axis('off')
+    # Read the uploaded image
+    image_path = '8.png'  # Replace with the path to your image
 
- # plt.tight_layout()
- # plt.show()
-         return inverted_image 
-        pre_img=preprocess()
-	prediction = model1.predict(np.array([pre_img]))
-        predicted_digit = np.argmax(prediction)
+    def preprocess():
+        gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        # Resize the image to 28x28 pixels (MNIST size)
+        resized_image = cv2.resize(gray_image, (28, 28))
+        # Invert the colors (black to white, white to black)
+        inverted_image = cv2.bitwise_not(resized_image)
+        return inverted_image
 
-        print(predicted_digit)
-	return 'Hello World'+str(predicted_digit)
+    pre_img = preprocess()
+    prediction = model1.predict(np.array([pre_img]))
+    predicted_digit = np.argmax(prediction)
 
+    print(predicted_digit)
+    return 'Hello World' + str(predicted_digit)
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
