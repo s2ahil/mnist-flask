1118092f981f07f683615699fec147a9241a772b
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-19 01:19:40 +0530

    Update mnist_backend.py

--- mnist_backend.py
+++ mnist_backend.py
@@ -17,7 +17,7 @@ def hello_world():
     json_file.close()
     model = model_from_json(loaded_model_json)
 
-    # Read the uploaded image
+    # Read the uploaded image model2
     image_path = '8.png'  # Replace with the path to your image
 
     def preprocess():
