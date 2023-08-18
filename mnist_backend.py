5d3629ca2d6419a8cb3f3a1d256a36940bc1ee1f
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-18 23:30:03 +0530

    Update mnist_backend.py

--- mnist_backend.py
+++ mnist_backend.py
@@ -7,16 +7,17 @@ import pickle
 import tensorflow.compat.v2 as tf
 from keras.models import model_from_json
 
+
 app = Flask(__name__)
 
 # with open('./mnist.pkl', 'rb') as model_file:
 #     model = pickle.load(model_file)
-# model = joblib.load('mnist.joblib')
+model = joblib.load('mnist.pkl')
 # model = load_model('mnist_h5')
-json_file = open('myModel.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-new_model = model_from_json(loaded_model_json)
+# json_file = open('myModel.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# new_model = model_from_json(loaded_model_json)
 
 
 @app.route('/',methods = ['GET'])
