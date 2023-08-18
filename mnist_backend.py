417bb4e23dbb6577c70b223344d47349d3529438
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-18 17:58:38 +0530

    Add files via upload

---
+++ mnist_backend.py
@@ -0,0 +1,20 @@
+
+from flask import Flask
+import numpy as np
+import joblib
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('mnist.joblib')
+# model = load_model('mnist_h5')
+
+
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
+
+
+if __name__ == '__main__':
+	app.run()
